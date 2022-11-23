d7a00be8de7481c85e0611c3b71b4d0052a9f752
Koen De Groot <koen.de.groot@organimmo.be>
2022-11-23 08:46:45 +0100

    added required properties

--- UploadingImagesToDatabaseWithApi/Shared/QrBuildingProblem.cs
+++ UploadingImagesToDatabaseWithApi/Shared/QrBuildingProblem.cs
@@ -1,12 +1,20 @@
-﻿namespace UploadingImagesToDatabaseWithApi.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UploadingImagesToDatabaseWithApi.Shared;
 
 public class QrBuildingProblem
 {
     public int Id { get; set; }
-    public string? BuildingName { get; set; }
-    public string? Description { get; set; }
-    public List<Image>? Images { get; set; } = new();
-    public string? UserName { get; set; }
-    public string? UserTelephone { get; set; }
-    public string? UserEmail { get; set; }
+
+    [Required] public string? BuildingName { get; set; }
+
+    [Required] public string? Description { get; set; }
+
+    [Required] public List<Image>? Images { get; set; } = new();
+
+    [Required] public string? UserName { get; set; }
+
+    [Required] public string? UserTelephone { get; set; }
+
+    [Required] public string? UserEmail { get; set; }
 }
